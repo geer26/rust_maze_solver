208fe3fc2a0779ab6cb82525903186861113c20e
geer26 <gergo.kurucz@gmail.com>
2022-02-22 15:14:20 +0100

    V0.1

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ enum Type{
     Floor,
     Node,
     Entrance,
-    None,
+    Undefined,
 }
 
 enum Parent{
@@ -50,9 +50,6 @@ fn main() {
         .read_line(&mut maze)
         .expect("Failed to read line");
 
-    //let maze = maze.;
-
-    //let contents = fs::read_to_string(maze)
     let contents = fs::read_to_string("to_text.txt")
         .expect("Something went wrong reading the file");
     println!("MAZE:\n{}", contents);
@@ -62,9 +59,6 @@ fn main() {
     for (r_index,row) in rows.into_iter().enumerate(){
         for (c_index, column) in row.chars().into_iter().enumerate(){
             println!("ROW - {} : COLUMN - {} :: VALUE - {}", r_index, c_index, column);
-            let mut ty: Type;
-            //let uuid = Uuid::
-            //if column == '#'{ ty = Type::Wall} else { ty = Type::Floor }
             let p = Point{
                 x: r_index,
                 y: c_index,
@@ -72,7 +66,7 @@ fn main() {
                 t: match column{
                     'o' => Type::Floor,
                     '#' => Type::Wall,
-                    //_ => (),
+                    _ => Type::Undefined,
                 },
 
                 //id: Uuid::new_v4().to_string(),
@@ -87,7 +81,7 @@ fn main() {
         println!("id:{}, x:{}, y:{}, type:{}", &p.id, &p.x, &p.y, match &p.t{
             Wall => "WALL",
             Floor => "FLOOR",
-            _ => (),
+            Undefined => "UNDEFINED",
         });
     }
 
